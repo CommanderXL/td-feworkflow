aefdce44e56f803329c2a1f70f34b0b27d456e38
XRene <xiaolei@didichuxing.com>
2016-08-07 21:57:18 +0800

    add the menu list in the app

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
  */
 
 var electron = require('electron');
+var Menu = electron.Menu;
 var app = electron.app;
 var browserWindow = electron.BrowserWindow;
 
@@ -22,8 +23,148 @@ function createWin() {
     });
 }
 
+function createMenu() {
+    var template = [
+        {
+            label: 'Edit',
+            submenu: [
+                {
+                    role: 'undo'
+                },
+                {
+                    role: 'redo'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    role: 'cut'
+                },
+                {
+                    role: 'copy'
+                },
+                {
+                    role: 'paste'
+                },
+                {
+                    role: 'pasteandmatchstyle'
+                },
+                {
+                    role: 'delete'
+                },
+                {
+                    role: 'selectall'
+                }
+            ]
+        },
+        {
+            label: 'View',
+            submenu: [
+                {
+                    label: 'Reload',
+                    accelerator: 'CmdOrCtrl+R',
+                    click: function (item, focusedWindow) {
+                        if(focusedWindow) focusedWindow.reload();
+                    }
+                },
+                {
+                    label: 'Toggle Developer Tools',
+                    accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+                    click: function (item, focusedWindow) {
+                        if(focusedWindow) focusedWindow.webContents.toggleDevTools();
+                    }
+                },
+                {
+                    role: 'togglefullscreen'
+                }
+            ]
+        },
+        {
+            role: 'window',
+            submenu: [
+                {
+                    role: 'minimize'
+                },
+                {
+                    role: 'close'
+                }
+            ]
+        },
+    ];
 
-app.on('ready', createWin);
+    if (process.platform === 'darwin') {
+        var name = app.getName();
+        template.unshift({
+            label: name,
+            submenu: [
+                {
+                    role: 'about'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    role: 'services',
+                    submenu: []
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    role: 'hide'
+                },
+                {
+                    role: 'hideothers'
+                },
+                {
+                    role: 'unhide'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    role: 'quit'
+                }
+            ]
+        });
+    }
+
+    // Window menu.
+    template[3].submenu = [
+        {
+            label: 'Close',
+            accelerator: 'CmdOrCtrl+W',
+            role: 'close'
+        },
+        {
+            label: 'Minimize',
+            accelerator: 'CmdOrCtrl+M',
+            role: 'minimize'
+        },
+        {
+            label: 'Zoom',
+            role: 'zoom'
+        },
+        {
+            type: 'separator'
+        },
+        {
+            label: 'Bring All to Front',
+            role: 'front'
+        }
+    ]
+
+    var menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+}
+
+
+app.on('ready', function () {
+    //打开窗口
+    createWin();
+    //添加菜单
+    createMenu();
+});
 
 
 app.on('window-all-closed', function () {
